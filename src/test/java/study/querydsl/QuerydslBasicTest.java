34a5f9efbc7edfe59cd2327d477babbcfa660969
지금이야 <cz2123xc@gmail.com>
2022-06-30 01:10:41 +0900

    조회 테스트코드 작성중

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,7 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QTeam.team;
 
 @SpringBootTest
 @Transactional
@@ -102,24 +105,166 @@ public class QuerydslBasicTest {
     }
 
 
+//    fetch() : 리스트 조회, 데이터 없으면 빈 리스트 반환
+//    fetchOne() : 단 건 조회
+//    결과가 없으면 : null
+//    결과가 둘 이상이면 : com.querydsl.core.NonUniqueResultException
+//    fetchFirst() : limit(1).fetchOne()
+//    fetchResults() : 페이징 정보 포함, total count 쿼리 추가 실행
+//    fetchCount() : count 쿼리로 변경해서 count 수 조
+
+    @Test // 조회
+    public void resultFetch() {
+
+        //List
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+        //단 건 (조회한 결과가 유일하지 않아 예외 발생 하므로 주석)
+//    Member findMember1 = queryFactory
+//            .selectFrom(member)
+//            .fetchOne();
+        //처음 한 건 조회
+        Member findMember2 = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+        //페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+        //count 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    // 정렬
+    /**
+     *
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순
+     * 2. 회원 이름 올림차순
+     * 단 2에서 회원 이름이 없으면 마지막에 출력 (null last)
+     *
+     */
+
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+    @Test // 조회 건수 제한 조회
+    public void paging1() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1) // 0 부터 시작 (zero index)
+                .limit(2) // 최대 2건 조회
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test // 전체 조회 수 (count 쿼리가 실행되므로 성능 주의)
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    /**
+     *
+     * JPQL
+     * select
+     * COUNT(m), // 회원수
+     * SUM(m.age), // 회원 나이 합계
+     * AVG(m.age), // 회원 나이 평균
+     * MAX(m.age), // 회원 나이 최대
+     * MIN(m.age), // 회원 나이 최소
+     * from Member m
+     *
+     */
+
+    @Test // 집합 함수
+    public void aggregation() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min())
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+
+    }
+
+    // 그룹바이 사용
+    /**
+     *
+     * 팀의 이름과 각 팀의 평균 연령을 구해라.
+     *
+     */
+
+    @Test
+    public void group() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name) // groupBy 결과를 제한하려면 having
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+
+    }
 
 
 
 
-//        member.username.eq("member1") // username = 'member1'
-//        member.username.ne("member1") //username != 'member1'
-//        member.username.eq("member1").not() // username != 'member1'
-//        member.username.isNotNull() //이름이 is not null
-//        member.age.in(10, 20) // age in (10,20)
-//        member.age.notIn(10, 20) // age not in (10, 20)
-//        member.age.between(10,30) //between 10, 30
-//        member.age.goe(30) // age >= 30
-//        member.age.gt(30) // age > 30
-//        member.age.loe(30) // age <= 30
-//        member.age.lt(30) // age < 30
-//        member.username.like("member%") //like 검색
-//        member.username.contains("member") // like ‘%member%’ 검색
-//        member.username.startsWith("member") //like ‘member%’ 검색
+
+
+
+
+
+
 
 
 
